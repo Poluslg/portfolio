3acfc4fa872f1894f646ce90e7df2d0d94069431
Poluslg <prahladbiswas08@gmail.com>
2023-05-05 01:04:35 +0530

    need to fix contact css

--- src/components/Base.jsx
+++ src/components/Base.jsx
@@ -7,8 +7,11 @@ const Base = () => {
   return (
     <>
       <Header />
-      <div id="content" className="sm:blur-0 z-[0] overflow-hidden scroll-smooth hover:scroll-auto md:scroll-auto" >
-        <div className="fixed top-0 h-[100%] w-full">
+      <div
+        id="content"
+        className="sm:blur-0 z-[0] overflow-hidden scroll-smooth hover:scroll-auto md:scroll-auto"
+      >
+        <div className="  absolute top-0 h-[100%] w-full">
           <Background />
         </div>
         <Outlet />
