f4a8bdeee54272dfd28575776e54ae3b332cb2a5
poluslg <prahladbiswas08@gmail.com>
2023-05-06 22:14:32 +0530

    asd

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,8 +6,13 @@ export const About = () => {
       id="About"
       className="bg-[#25CCF7] h-screen  dark:bg-gradient-to-r from-black to-polu-500"
     >
-      <div className="text-black text-center">
-        <h1>About</h1>
+      <div className="text-black font-sans font-[50px]">
+        <a className="text-7xl font-bold tracking-tight text-transparent bg-gray-800    dark:text-transparent  bg-clip-text">
+          About Me
+        </a>
+        <a href="">
+          Hi there! My name is Prahlad Biswas, and I'm a student pursuing a Bachelor's degree in Information Technology. I have a strong passion for technology and am always eager to learn about the latest advancements in the field. Apart from my studies, I enjoy exploring different programming languages and software development tools. In my free time, I like to experiment with coding projects and create new software solutions. My goal is to gain the skills and knowledge necessary to become a successful software engineer, and I'm excited to see where my education and passion for technology will take me in the future!
+        </a>
       </div>
     </div>
   );
